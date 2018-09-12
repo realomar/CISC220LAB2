c9bab68631a6cdff0e7d26efee1de1472dad5b05
Omar Ahmad <eatmorerice@outlook.com>
2018-09-12 12:25:44 -0400

    created file

---
+++ LAB2.cpp
@@ -0,0 +1,17 @@
+/*
+ * CISC220 LAB 2
+ * Debra Yarrington
+ *
+ * Created on: Sept 12, 2018
+ *
+ * Authors: omaromar@udel.edu Omar Ahmad
+ *  		tamimoz@udel.edu  Ahmad Tamimi
+ *
+ * This file contains functions for lab 2. The functions aren't related
+ * in any other way that they are required for lab 2.
+ */
+#include <iostream> //imports input/output
+#include <stdlib.h> //includes std library
+using namespace std;
+
+int main(){}
